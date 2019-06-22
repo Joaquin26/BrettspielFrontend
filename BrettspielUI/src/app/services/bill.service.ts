07309f641495cff3904f4cfec16bb37125bf60a1
Diego Castillo <raigiku@hotmail.com>
2019-06-22 05:47:35 -0500

    Se creo bill service

---
+++ BrettspielUI/src/app/services/bill.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Bill} from '../model/Bill';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BillService {
+
+  private baseURL = 'http://localhost:8080/api/bill';
+
+  constructor(private  http: HttpClient) { }
+
+  saveBill(bill: Bill): Observable<object> {
+    return this.http.post(`${this.baseURL}`, bill);
+  }
+}
